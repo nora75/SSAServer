f781d4165d560a3f0d6732ee6922e222ea444dfb
Hirano Tatsuya <k017c1066@it-neec.jp>
2019-06-19 11:53:15 +0900

    gitignoreの追加、APIのデザインの変更
    [API]
    * APIのデザインを大幅に変更
    * idやグループidを元にエンドポイントの設計
        * グループid生成、ログイン、アカウント削除はidを元に
        * 各種データのやりとりはgroup_idを元に

--- design/define.go
+++ design/define.go
@@ -4,27 +4,106 @@ import (
 	. "goa.design/goa/v3/dsl"
 )
 
-var BasicAuth = BasicAuthSecurity("basic_auth")
+// BasicAuth defines a security scheme using basic authentication. The scheme
+// protects the "signin" action used to create JWTs.
+var BasicAuth = BasicAuthSecurity("basic", func() {
+	Description("Basic authentication used to authenticate security principal during signin")
+	Scope("api:read", "Read-only access")
+})
 
+// MyResultType is declation of use in result views
 var MyResultType = ResultType("application/vnd.goa.ssa", func(){
 	ContentType("application/json")
 
 	Attributes(func() {
-		Attribute("user_name", String, "User Name")
-		Attribute("password", String, "User Password")
-		Attribute("mail", String, "User mail-address")
-		Attribute("group_id", String, "Gourp ID")
-		Attribute("data_name", String, "Data name")
-		Attribute("Data", Any, "Data name")
+		Attribute("user_id", String, "User Name", func(){
+			Example("The first user id", func(){
+				Description("The user who has register first")
+				Value(Val{"id": 1})
+			})
+			Example("The 234's user id", func(){
+				Description("The user who has register 234's")
+				Value(Val{"id": 234})
+			})
+		})
+		Attribute("user_name", String, "User Name", func(){
+			Example("The user's name Hirano", func(){
+				Description("Simple usecase")
+				Value(Val{"user_name": "Hirano"})
+			})
+			Example("The user's name Hirano Tatsuya", func(){
+				Description("Can contain space")
+				Value(Val{"user_name": "Hirano Tatsuya"})
+			})
+			Example("The user's name 平野 竜也", func(){
+				Description("Can use utf-8")
+				Value(Val{"user_name": "平野 竜也"})
+			})
+		})
+		Attribute("password", String, "User Password", func(){
+			Example("The password is 'password'", func(){
+				Description("The password is 'password' but shoudn't use it")
+				Value(Val{"password": "password"})
+			})
+			Example("The password is 'PassW0rd!!@'", func(){
+				Description("The password is 'PassW0rd!!@'")
+				Value(Val{"password": "PassW0rd!!@"})
+			})
+		})
+		Attribute("mail", String, "User mail-address", func(){
+			Example("Example of mail-address", func(){
+				Description("Example of mail-address")
+				Value(Val{"mail": "hoge@hoge.com"})
+			})
+		})
+		Attribute("group_id", String, "Gourp ID", func(){
+			Example("The group name is 'bzuiphjjgdas'", func(){
+				Description("The user who belong to Group 'bzuiphjjgdas'")
+				Value(Val{"gourp_id": "bzuiphjjgdas"})
+			})
+		})
+		Attribute("data_name", String, "Data name", func(){
+			Example("The example of record data", func(){
+				Description("The record data that's name is 'Record_12_2019-06-02_12-07-35'")
+				Value(Val{"data_name": "Record_12_2019-06-02_12-07-35"})
+			})
+			Example("The example of Diary data", func(){
+				Description("The diary data that's name is 'diary_312_2019-03-02_12-07-35'")
+				Value(Val{"data_name": "Record_312_2019-03-02_12-07-35"})
+			})
+		})
+		Attribute("Data", Any, "Data")
+		Attribute("data_type", String, "Data's name", func(){
+			Example("The record", func(){
+				Description("The data_type 0(record)")
+				Value(Val{"data_type": 0})
+			})
+			Example("The Diary", func(){
+				Description("The data_type 1(diary)")
+				Value(Val{"data_type": 1})
+			})
+		})
 	})
 
 	View("default", func() {
-		// "id" and "name" must be result type attributes
 		Attribute("mail")
 		Attribute("user_name")
 	})
 
 	View("extended", func() {
+		Attribute("mail")
+		Attribute("user_name")
+		Attribute("group_id")
+	})
+
+	View("login", func() {
+		Attribute("user_id")
+		Attribute("mail")
+		Attribute("user_name")
+	})
+
+	View("extended", func() {
+		Attribute("user_id")
 		Attribute("mail")
 		Attribute("user_name")
 		Attribute("group_id")
@@ -36,6 +115,7 @@ var MyResultType = ResultType("application/vnd.goa.ssa", func(){
 	
 	View("data", func() {
 		Attribute("Data")
+		Attribute("data_type")
 	})
 })
 
@@ -60,6 +140,7 @@ var _ = Service("SSAServer", func() {
 	Error("Invalid User Name")
 	Error("Invalid data_name")
 	Error("Invalid mail-address")
+	Error("Require Authentication")
 	Error("The user already exists")
 
 	Method("Register", func() {
@@ -69,22 +150,29 @@ var _ = Service("SSAServer", func() {
 			Attribute("mail", String, "User mail-address")
 			Attribute("group_id", String, "Gourp ID")
 			Required("user_name", "password", "mail")
+			Example("Register example without group_id", func(){
+				Description("The sample of register")
+				Value(Val{"user_name": "hoge",
+					"password": "password",
+					"mail": "hoge@hoge.com"})
+			})
+			Example("Register example with group_id", func(){
+				Description("The sample of register")
+				Value(Val{"user_name": "hoge",
+					"password": "password",
+					"mail": "hoge@hoge.com",
+					"group_id": "group1"})
+			})
 		})
 
-		Result(MyResultType, func(){
-			View("default")
-		})
-
-		Result(MyResultType, func(){
-			View("extended")
-			Required("group_id")
-		})
+		Result(Boolean)
 
 		HTTP(func() {
-			POST("/Register")
+			GET("/Register")
 			Response(StatusOK)
 			Response("Invalid Group ID", StatusNotFound)
 			Response("The user already exists", StatusBadRequest)
+			Response("Invalid Request", StatusBadRequest)
 		})
 
 	})
@@ -96,90 +184,42 @@ var _ = Service("SSAServer", func() {
 			Password("password", String)
 			Attribute("group_id", Int, "Gourp ID")
 			Required("mail", "password")
+			Example("Login example without group_id", func(){
+				Description("The sample of login")
+				Value(Val{"mail": "hoge@hoge.com",
+					"password": "password"})
+			})
+			Example("Login example with group_id", func(){
+				Description("The sample of login")
+				Value(Val{"mail": "hoge@hoge.com",
+					"password": "password",
+					"group_id": "group1"})
+			})
 		})
 
 		Result(MyResultType, func(){
-			View("default")
+			View("login")
 		})
 
 		Result(MyResultType, func(){
-			View("extended")
+			View("login_extended")
 			Required("group_id")
 		})
 
 		HTTP(func() {
-			GET("/Login")
+			POST("/{id}")
 			Response(StatusOK)
-			Response("Invalid Group ID", StatusNotFound)
-			Response("Invalid Password", StatusBadRequest)
-			Response("Invalid User Name", StatusBadRequest)
-		})
-
-	})
-
-	Method("Join", func(){
-		Payload(func(){
-			Attribute("id", Int, "User ID")
-			Attribute("group_id", String, "Group ID")
-		})
-
-		Result(Boolean)
-
-		HTTP(func(){
-			GET("/Join")
-			Response("Invalid Group ID", StatusBadRequest)
-			Response(StatusOK)
-		})
-	})
-
-	Method("Save", func() {
-		Payload(func() {
-			Attribute("id", Int, "User ID")
-			Attribute("group_id", String, "Gourp ID")
-			Attribute("data_name", String, "Data name")
-			Attribute("Data", Any, "Data name")
-			Required("id", "group_id", "data_name", "Data")
-		})
-
-		Result(Boolean)
-
-		HTTP(func() {
-			GET("/Save/{id}")
-			Response("Invalid Password", StatusBadRequest)
 			Response("Invalid Request", StatusBadRequest)
-			Response("Invalid User Name", StatusBadRequest)
 			Response("Invalid Group ID", StatusNotFound)
-			Response(StatusOK)
 		})
 
 	})
 
-	Method("PickUp", func() {
+	Method("Generate_group_id", func() {
 		Payload(func() {
-			Attribute("id", Int, "User ID")
-			Attribute("group_id", String, "Gourp ID")
-			Attribute("data_name", String, "Data name")
-			Required("id", "group_id", "data_name")
-		})
-
-		Result(MyResultType, func(){
-			View("data")
-		})
-
-		HTTP(func() {
-			GET("/PickUp")
-			Response(StatusOK)
-			Response("Invalid Group ID", StatusNotFound)
-			Response("Invalid Request", StatusBadRequest)
-			Response("Invalid data_name", StatusBadRequest)
-		})
-
-	})
-
-	Method("Generate", func() {
-		Payload(func() {
-			Attribute("id", Int, "User ID")
-			Attribute("mail", Int, "User mail-address")
+			Security(BasicAuth)
+			Username("id", Int, "User ID")
+			Password("password", Int, "User password")
 			Required("id", "mail")
 		})
 
@@ -188,11 +228,105 @@ var _ = Service("SSAServer", func() {
 		})
 
 		HTTP(func() {
-			GET("/GroupID/{id}")
+			GET("/{id}")
 			Response(StatusOK)
 			Response("Invalid ID", StatusBadRequest)
-			Response("Invalid mail-address", StatusBadRequest)
-			Response("Invalid Request", StatusForbidden)
+			Response("Invalid Request", StatusBadRequest)
+		})
+
+	})
+
+	Method("Join_group", func(){
+		Payload(func(){
+			Attribute("id", Int, "User ID")
+			Attribute("mail", String, "User mail-address")
+			Attribute("group_id", String, "Group ID")
+			Required("id", "mail","group_id")
+			Example("Join group1", func(){
+				Description("Join group1")
+				Value(Val{"id": "123",
+					"password": "password",
+					"group_id": "group1"})
+			})
+			Example("Join group2", func(){
+				Description("Join group1")
+				Value(Val{"id": "3123",
+					"password": "password",
+					"group_id": "group2"})
+			})
+		})
+
+		Result(Boolean)
+
+		HTTP(func(){
+			POST("/{id}/Join")
+			Response("Invalid Group ID", StatusBadRequest)
+			Response("Invalid Request", StatusBadRequest)
+			Response(StatusOK)
+		})
+	})
+
+	Method("Delete_user", func(){
+		Payload(func(){
+		Security(BasicAuth)
+			Username("id", Int)
+			Password("password", String)
+			Required("id", "password")
+			Example("Delete user 3123", func(){
+				Description("Delete user who's id is 3123")
+				Value(Val{"password": "password"})
+			})
+		})
+
+		Result(Boolean)
+
+		HTTP(func(){
+			DELETE("/{id}")
+			Response("Invalid Request", StatusBadRequest)
+			Response(StatusOK)
+		})
+	})
+
+	Method("Save", func() {
+		Payload(func() {
+			Attribute("group_id", String, "Gourp ID")
+			Attribute("id", Int, "User ID")
+			Attribute("data_name", String, "Data name")
+			Attribute("data_type", String, "Data name")
+			Attribute("Data", Any, "Data name")
+			Required("group_id", "id", "data_name", "Data")
+		})
+
+		Result(Boolean)
+
+		HTTP(func() {
+			POST("/{group_id}")
+			Response("Invalid Group ID", StatusNotFound)
+			Response("Invalid Request", StatusBadRequest)
+			Response("Invalid Data", StatusBadRequest)
+			Response(StatusOK)
+		})
+
+	})
+
+	Method("Pick_up", func() {
+		Payload(func() {
+			Attribute("group_id", String, "Gourp ID")
+			Attribute("id", Int, "User ID")
+			Attribute("data_name", String, "Data name")
+			Required("group_id", "id", "data_name")
+		})
+
+		Result(MyResultType, func(){
+			View("data")
+		})
+
+		HTTP(func() {
+			GET("/{group_id}")
+			Response(StatusOK)
+			Response("Invalid Group ID", StatusNotFound)
+			Response("Invalid Request", StatusBadRequest)
+			Response("Invalid data_name", StatusBadRequest)
 		})
 
 	})
