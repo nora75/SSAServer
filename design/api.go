e345683cc17e7d50046cdda09f7757d7f787b323
Hirano Tatsuya <k017c1066@it-neec.jp>
2019-06-22 18:57:08 +0900

    APIの仕様変更,Swaggerでのエラー修正
    * /{id} -> /users/{user_id}
    + 不要な詳細の消去と共に日本語に

--- design/api.go
+++ design/api.go
@@ -9,90 +9,90 @@ var MyResultType = ResultType("application/vnd.goa.ssa", func(){
 	ContentType("application/json")
 
 	Attributes(func() {
-		Attribute("user_id", MapOf(String, Int), "User id", func(){
+		Attribute("user_id", Int, "User id", func(){
 			Example("The first user id", func(){
 				Description("The user who has register first")
-				Value(map[string]int{"id": 1})
+				Value(1)
 			})
 			Example("The 234's user id", func(){
 				Description("The user who has register 234's")
-				Value(map[string]int{"id": 234})
+				Value(234)
 			})
 		})
-		Attribute("user_name", MapOf(String, String), "User Name", func(){
+		Attribute("user_name", String, "User Name", func(){
 			Example("The user's name Hirano", func(){
 				Description("Simple usecase")
-				Value(map[string]string{"user_name": "Hirano"})
+				Value("Hirano")
 			})
 			Example("The user's name Hirano Tatsuya", func(){
 				Description("Can contain space")
-				Value(map[string]string{"user_name": "Hirano Tatsuya"})
+				Value("Hirano Tatsuya")
 			})
 			Example("The user's name 平野 竜也", func(){
 				Description("Can use utf-8")
-				Value(map[string]string{"user_name": "平野 竜也"})
+				Value("平野 竜也")
 			})
 		})
-		Attribute("password", MapOf(String, String), "User Password", func(){
+		Attribute("password", String, "User Password", func(){
 			Example("The password is 'password'", func(){
 				Description("The password is 'password' but shoudn't use it")
-				Value(map[string]string{"password": "password"})
+				Value("password")
 			})
 			Example("The password is 'PassW0rd!!@'", func(){
 				Description("The password is 'PassW0rd!!@'")
-				Value(map[string]string{"password": "PassW0rd!!@"})
+				Value("PassW0rd!!@")
 			})
 		})
-		Attribute("mail", MapOf(String, String), "User mail-address", func(){
+		Attribute("mail", String, "User mail-address", func(){
 			Example("Example of mail-address", func(){
 				Description("Example of mail-address")
-				Value(map[string]string{"mail": "hoge@hoge.com"})
+				Value("hoge@hoge.com")
 			})
 		})
-		Attribute("group_id", MapOf(String, String), "Gourp ID", func(){
+		Attribute("group_id", String, "Gourp ID", func(){
 			Example("The group name is 'bzuiphjjgdas'", func(){
 				Description("The user who belong to Group 'bzuiphjjgdas'")
-				Value(map[string]string{"gourp_id": "bzuiphjjgdas"})
+				Value("bzuiphjjgdas")
 			})
 		})
-		Attribute("data_name", MapOf(String, String), "Data name", func(){
+		Attribute("data_name", String, "Data name", func(){
 			Example("The example of record data", func(){
 				Description("Record data of User 12,Time 2019-06-02_12-07-35")
-				Value(map[string]string{"data_name": "Record_12_2019-06-02_12-07-35"})
+				Value("Record_12_2019-06-02_12-07-35")
 			})
 			Example("The example of Diary data", func(){
 				Description("Diary data of User 312,Time 2019-03-02_12-07-35")
-				Value(map[string]string{"data_name": "Diary_312_2019-03-02_12-07-35"})
+				Value("Diary_312_2019-03-02_12-07-35")
 			})
 		})
 		Attribute("Data", Any, "Data")
 		Attribute("Image", Any, "Image data")
-		Attribute("data_type", MapOf(String, Int), "Data's name", func(){
+		Attribute("data_type", Int, "Data's name", func(){
 			Example("The record", func(){
 				Description("The data_type 0(record)")
-				Value(map[string]int{"data_type": 0})
+				Value(0)
 			})
 			Example("The Diary", func(){
 				Description("The data_type 1(diary)")
-				Value(map[string]int{"data_type": 1})
+				Value(1)
 			})
 		})
-		Attribute("title", MapOf(String, String), "Data title", func(){
+		Attribute("title", String, "Data title", func(){
 			Example("Title: hoge", func(){
 				Description("The Diary's title of hoge")
-				Value(map[string]string{"title": "hoge"})
+				Value("hoge")
 			})
 		})
-		Attribute("image_name", MapOf(String, String), "image's name", func(){
+		Attribute("image_name", String, "image's name", func(){
 			Example("Sample image name", func(){
 				Description("The image's title of huge")
-				Value(map[string]string{"image_name": "huge"})
+				Value("huge")
 			})
 		})
-		Attribute("date_time", MapOf(String, String), "date time", func(){
+		Attribute("date_time", String, "date time", func(){
 			Example("date time", func(){
 				Description("uploaded time")
-				Value(map[string]string{"date_time": "2019-06-05-02-02"})
+				Value("2019-06-05-02-02")
 			})
 		})
 		Attribute("data_list_origin", Any, "data_list_recursive")
@@ -102,6 +102,14 @@ var MyResultType = ResultType("application/vnd.goa.ssa", func(){
 		Attribute("user_id")
 		Attribute("mail")
 		Attribute("user_name")
+		Example("example", func(){
+			Description("example of default")
+			Value(map[string]string{
+				"user_id": "1",
+				"user_name": "Hirano",
+				"mail": "Hirano@test.com",
+			})
+		})
 	})
 
 	View("extended", func() {
--- design/service.go
+++ design/service.go
@@ -5,37 +5,67 @@ import (
 )
 
 var _ = Service("SSAServer", func() {
-	Description("Service for account management.")
-	Error("Invalid ID")
-	Error("Invalid Password")
-	Error("Invalid Group ID")
-	Error("Invalid Request")
-	Error("Invalid User Name")
-	Error("Invalid data_name")
-	Error("Invalid Data")
-	Error("Invalid mail-address")
-	Error("Require Authentication")
-	Error("The user already exists")
+	Description("SSAアプリケーションのサーバーサイド")
+	Error("Invalid_User_ID")
+	Error("Invalid_Password")
+	Error("Invalid_Group_ID")
+	Error("Invalid_Request")
+	Error("Invalid_User_Name")
+	Error("Invalid_data_name")
+	Error("Invalid_Data")
+	Error("Invalid_mail-address")
+	Error("Require_Authentication")
+	Error("The_user_already_exists")
 
 	Method("Register", func() {
+		Description("SSAへの新規登録")
 		Payload(func() {
-			Attribute("user_name", String, "User Name")
-			Attribute("password", String, "User Password")
-			Attribute("mail", String, "User mail-address")
-			Attribute("group_id", String, "Gourp ID")
-			Required("user_name", "password", "mail")
-			Example("Register example without group_id", func(){
-				Description("The sample of register")
-				Value(map[string]string{"user_name": "hoge",
-					"password": "password",
-					"mail": "hoge@hoge.com"})
+			Attribute("user_name", String, "User Name", func(){
+				Example("The user's name Hirano Tatsuya", func(){
+					Description("単純な例")
+					Value("Hirano Tatsuya")
+				})
+				Example("The user's name 平野 竜也", func(){
+					Description("UTF-8に対応")
+					Value("平野 竜也")
+				})
 			})
-			Example("Register example with group_id", func(){
-				Description("The sample of register")
-				Value(map[string]string{"user_name": "hoge",
+			Attribute("password", String, "User Password", func(){
+				Example("The password is 'password'", func(){
+					Description("'password'がパスワードの場合")
+					Value("password")
+				})
+				Example("The password is 'PassW0rd!!@'", func(){
+					Description("'PassW0rd!!@'がパスワードの場合")
+					Value("PassW0rd!!@")
+				})
+			})
+			Attribute("mail", String, "User mail-address", func(){
+				Example("Example of mail-address", func(){
+					Description("メアド")
+					Value("hoge@hoge.com")
+				})
+			})
+			Attribute("group_id", String, "Gourp ID", func(){
+				Example("'bzuiphjjgdas'がグループ名の場合", func(){
+					Value("bzuiphjjgdas")
+				})
+			})
+			Required("user_name", "password", "mail")
+			Example("group_id抜きの例", func(){
+				Value(map[string]string{
+					"user_name": "hoge",
 					"password": "password",
 					"mail": "hoge@hoge.com",
-					"group_id": "group1"})
+				})
+			})
+			Example("group_id付きの例", func(){
+				Value(map[string]string{
+					"user_name": "hoge",
+					"password": "password",
+					"mail": "hoge@hoge.com",
+					"group_id": "group1",
+				})
 			})
 		})
 
@@ -51,27 +81,26 @@ var _ = Service("SSAServer", func() {
 		HTTP(func() {
 			POST("/Registration")
 			Response(StatusOK)
-			Response("Invalid Group ID", StatusNotFound)
-			Response("The user already exists", StatusBadRequest)
-			Response("Invalid Request", StatusBadRequest)
+			Response("Invalid_Group_ID", StatusNotFound)
+			Response("The_user_already_exists", StatusBadRequest)
+			Response("Invalid_Request", StatusBadRequest)
 		})
 
 	})
 
 	Method("Login", func() {
+		Description("SSAへのログイン")
 		Payload(func() {
-			Username("mail", String)
-			Password("password", String)
-			Attribute("group_id", Int, "Gourp ID")
-			Attribute("id", Int, "User ID")
+			Attribute("mail", MapOf(String, String), "User mail-address")
+			Attribute("password", MapOf(String, String), "User Password")
+			Attribute("group_id", MapOf(String, Int), "Gourp ID")
+			Attribute("user_id", MapOf(String, Int), "User ID")
 			Required("mail", "password")
-			Example("Login example without group_id", func(){
-				Description("The sample of login")
+			Example("group_id抜きの場合", func(){
 				Value(map[string]string{"mail": "hoge@hoge.com",
 					"password": "password"})
 			})
-			Example("Login example with group_id", func(){
-				Description("The sample of login")
+			Example("group_id付きの場合", func(){
 				Value(map[string]string{"mail": "hoge@hoge.com",
 					"password": "password",
 					"group_id": "group1"})
@@ -90,86 +119,83 @@ var _ = Service("SSAServer", func() {
 		HTTP(func() {
 			POST("/Login")
 			Response(StatusOK)
-			Response("Invalid Request", StatusBadRequest)
-			Response("Invalid Group ID", StatusNotFound)
+			Response("Invalid_Request", StatusBadRequest)
+			Response("Invalid_Group_ID", StatusNotFound)
 		})
 
 	})
 
-	Method("Join_group", func(){
+	Method("Change_group", func(){
+		Description("グループIDを変更する場合")
 		Payload(func(){
-			Username("id", Int, "User ID")
-			Password("password", String, "User password")
-			Attribute("group_id", String, "Group ID")
-			Required("id", "password","group_id")
-			Example("Join group1", func(){
-				Description("Join group1")
-				Value(map[string]string{"id": "123",
+			Attribute("user_id", Int, "User ID")
+			Attribute("password", MapOf(String, String), "User password")
+			Attribute("group_id", MapOf(String, String), "Group ID")
+			Required("user_id", "password","group_id")
+			Example("'group2'へ変更", func(){
+				Value(map[string]string{
 					"password": "password",
-					"group_id": "group1"})
-			})
-			Example("Join group2", func(){
-				Description("Join group1")
-				Value(map[string]string{"id": "3123",
-					"password": "password",
-					"group_id": "group2"})
+					"group_id": "group2",
+				})
 			})
 		})
 
 		HTTP(func(){
-			POST("/{id}")
-			Response("Invalid Group ID", StatusBadRequest)
-			Response("Invalid Request", StatusBadRequest)
+			POST("/users/{user_id}")
+			Response("Invalid_Group_ID", StatusBadRequest)
+			Response("Invalid_Request", StatusBadRequest)
 			Response(StatusOK)
 		})
 	})
 
 	Method("Delete_user", func(){
+		Description("既存ユーザーの消去")
 		Payload(func(){
-			Username("id", Int)
-			Password("password", String)
-			Required("id", "password")
-			Example("Delete user 3123", func(){
-				Description("Delete user who's id is 3123")
+			Attribute("user_id", Int, "User ID")
+			Attribute("password", MapOf(String, String), "User Password")
+			Required("user_id", "password")
+			Example("ユーザー:'3123'の消去", func(){
 				Value(map[string]string{"password": "password"})
 			})
 		})
 
 		HTTP(func(){
-			DELETE("/{id}")
-			Response("Invalid Request", StatusBadRequest)
+			DELETE("/users/{user_id}")
+			Response("Invalid_Request", StatusBadRequest)
 			Response(StatusOK)
 		})
 	})
 
 	Method("Save", func() {
+		Description("データをサーバーへ保存する")
 		Payload(func() {
 			Attribute("group_id", String, "Gourp ID")
-			Attribute("id", Int, "User ID")
-			Attribute("data_name", String, "Data name")
-			Attribute("data_type", Int, "Data name")
-			Attribute("Data1", Any, "Data1")
-			Attribute("title", String, "Diary title")
-			Attribute("image_name", String, "Image name")
-			Attribute("Data2", Any, "Data2")
-			Required("group_id", "id", "data_name", "Data1")
+			Attribute("user_id", MapOf(String, Int), "User ID")
+			Attribute("data_name", MapOf(String, String), "Data name")
+			Attribute("data_type", MapOf(String, Int), "Data name")
+			Attribute("Data1", MapOf(String, Any), "Data1")
+			Attribute("title", MapOf(String, String), "Diary title")
+			Attribute("image_name", MapOf(String, String), "Image name")
+			Attribute("Data2", MapOf(String, Any), "Data2")
+			Required("group_id", "user_id", "data_name", "Data1")
 		})
 
 		HTTP(func() {
-			POST("/{group_id}")
-			Response("Invalid Group ID", StatusNotFound)
-			Response("Invalid Request", StatusBadRequest)
-			Response("Invalid Data", StatusBadRequest)
+			POST("/group/{group_id}")
+			Response("Invalid_Group_ID", StatusNotFound)
+			Response("Invalid_Request", StatusBadRequest)
+			Response("Invalid_Data", StatusBadRequest)
 			Response(StatusOK)
 		})
 
 	})
 
 	Method("Return_data_list", func() {
+		Description("データのリストを取得する")
 		Payload(func() {
 			Attribute("group_id", String, "Gourp ID")
-			Attribute("id", Int, "User ID")
-			Required("group_id", "id")
+			Attribute("user_id", MapOf(String, Int), "User user_ID")
+			Required("group_id", "user_id")
 		})
 
 		Result(MyResultType, func(){
@@ -177,21 +203,22 @@ var _ = Service("SSAServer", func() {
 		})
 
 		HTTP(func() {
-			GET("/{group_id}")
+			GET("/group/{group_id}")
 			Response(StatusOK)
-			Response("Invalid Group ID", StatusNotFound)
-			Response("Invalid Request", StatusBadRequest)
+			Response("Invalid_Group_ID", StatusNotFound)
+			Response("Invalid_Request", StatusBadRequest)
 		})
 
 	})
 
 	Method("Pick_up", func() {
+		Description("データをサーバーから取得する")
 		Payload(func() {
 			Attribute("group_id", String, "Gourp ID")
-			Attribute("id", Int, "User ID")
-			Attribute("data_name", String, "Data name")
-			Attribute("data_type", String, "Data type")
-			Required("group_id", "id", "data_name")
+			Attribute("user_id", MapOf(String, Int), "User ID")
+			Attribute("data_name", MapOf(String, String), "Data name")
+			Attribute("data_type", MapOf(String, String), "Data type")
+			Required("group_id", "user_id", "data_name")
 		})
 
 		Result(MyResultType, func(){
@@ -209,11 +236,11 @@ var _ = Service("SSAServer", func() {
 		})
 
 		HTTP(func() {
-			GET("/{group_id}")
+			GET("/group/{group_id}")
 			Response(StatusOK)
-			Response("Invalid Group ID", StatusNotFound)
-			Response("Invalid Request", StatusBadRequest)
-			Response("Invalid data_name", StatusBadRequest)
+			Response("Invalid_Group_ID", StatusNotFound)
+			Response("Invalid_Request", StatusBadRequest)
+			Response("Invalid_data_name", StatusBadRequest)
 		})
 
 	})
