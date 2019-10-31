75bf48645b13561ca8ebf9e565f9b179414fe4f5
Hirano Tatsuya <k017c1066@it-neec.jp>
2019-10-31 21:00:20 +0900

    リストを返せるように。

--- Gin/gin.go
+++ Gin/gin.go
@@ -20,15 +20,46 @@ type Test struct {
 	DataName string `form:"data_name" json:"data_name" xml:"data_name"  binding:"required"`
 }
 
+// RetListReq Binding from JSON
+type RetListReq struct {
+	UserID   string `form:"user_id" json:"user_id" xml:"user_id"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
 func main() {
 	r := gin.Default()
+	r.GET("/group/:group_id", func(c *gin.Context) {
+		groupID := c.Param("group_id")
+		// json parse sect
+		var json RetListReq
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		userID, err := strconv.Atoi(json.UserID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		password := json.Password
+		err = db.GroupCheck(userID, password, groupID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		datalist, err := db.FindAllDataInGroup(groupID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, datalist)
+	})
 	r.GET("/group/:group_id/:data_user_id", func(c *gin.Context) {
 		groupID := c.Param("group_id")
 		dataUserID, err := strconv.Atoi(c.Param("data_user_id"))
 		if err != nil {
-			fmt.Println("error in datauser id")
-			fmt.Println(err)
-			os.Exit(1)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		// json parse sect
 		var json Test
@@ -38,33 +69,35 @@ func main() {
 		}
 		userID, err := strconv.Atoi(json.UserID)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		password := json.Password
 		dataName := json.DataName
 		err = db.GroupCheck(userID, password, groupID)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		path, err := files.GetPickUpPath(groupID,dataUserID,dataName)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		b, err := ioutil.ReadFile(path)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		file, err := os.Open(path)
 		if err != nil {
-			panic("hoge")
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		fi, err := file.Stat()
 		if err != nil {
-			panic("hoge")
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
 		}
 		defer file.Close()
 		contentLength := fi.Size()
